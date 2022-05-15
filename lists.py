5ca390b53f177961f6e1f9e9d4b1023f9d11ecd7
Ralaam1 <a.s.malafeev@gmail.com>
2022-05-15 18:46:02 +0300

    Сделал тестовые читабельнее одним файлом с последовательным выполнением

--- lists.py
+++ lists.py
@@ -1,5 +1,15 @@
 numders = [3,5,7,9,10.5]
-numders.append('Python')
+print(numders)              #1
+
+numders.append('Python')    #2
 print(numders)
-numders.remove('Python')
+
+print(numders[0])           #3
+
+print(numders[-1])          #4
+
+print(numders[1:5])         #5
+
+
+numders.remove('Python')    #6
 print(numders)
